f16779528bded2a35593645f775f0da7d9871104
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-02-19 20:38:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-apt.get('/article-two',function(req,res){
-   res.send('Article-two is requested'); 
-});
-apt.get('/article-three',function(req,res){
-   res.send('Article-three is requested'); 
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
