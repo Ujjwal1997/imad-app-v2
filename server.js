b2124be03f03480b50de0f294e83db0e483aa12e
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-02-22 20:35:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,6 +10,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/poetry', function (req, res) {
+  res.send('Poetry page');
+});
+
+app.get('/Lifestyle', function (req, res) {
+  res.send('Lifestyle page');
+});
+
+app.get('/Relationship', function (req, res) {
+  res.send('Relationship page');
+});
+
+app.get('/technology', function (req, res) {
+  res.send('technology');
+});
 
 
 app.get('/ui/style.css', function (req, res) {
