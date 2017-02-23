6cec36ad512b85ed3144dd134dffab82a5049125
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-02-23 12:13:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,6 +42,9 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/logo.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'logo.png'));
 });
+app.get('/ui/images.jpeg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'images.jpeg'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
