0711ba58f3e202097fa969be660079d4da4e1e08
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-02-23 16:21:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,11 @@
 console.log('Loaded!');
 
-var myname = document.getElementById('name');
+var img = document.getElementById('name');
 
-var marginleft = 0;
-function moveleft(){
-    marginleft = marginleft + 10;
-    image.style.marginleft= marginleft + 'px';
-}
-myname.Onclick = function(){
+
+img.Onclick = function(){
    // var interval = setintervat(moveleft,100);
-    myname.style.marginleft= '100px';
+    marginleft= '200px';
 };
 
 
