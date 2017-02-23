8a73db0789b4cedf1681972aa3dba0973211bb9f
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-02-23 16:34:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/ui/movies', function (req, res) {
 var counter=0;
 apt.get('/counter', function(req,res){
    counter = counter +1;
-   res.send(counter.toString());
+   res.send('counter.toString()');
 });
 
 app.get('/ui/Lifestyle', function (req, res) {
