8615dad5526a08944f1fcf84559b0a09eed7db2b
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-03-18 10:05:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,8 @@
-console.log('Loaded!');
-
-var img = document.getElementById('madi');
-
-var counter = 0;
-
-img.Onclick = function() {
-    img.style.marginleft = '100px';
-};
-
 var button = document.getElementById('counter');
-
+var z = 0;
 button.Onclick = function() {
-    counter = counter + 1;
+    z = z + 1;
     
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-    
 };
