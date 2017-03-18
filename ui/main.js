6f8f3daa007c058b581bad88bc0f366b09b5a28b
Ujjwal1997 <ujjwal.ranjan@st.niituniversity.in>
2017-03-18 10:16:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,7 @@
 var button = document.getElementById("counter");
-var z = 0;
 button.Onclick = function() {
-    z = z + 1;
+
     
     var span = document.getElementById("count");
-    span.innerHTML = z.toString();
+    span.innerHTML = "ujjwal";
 };
